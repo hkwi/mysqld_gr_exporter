75b21e7ea727722227e426b535158c87344b8911
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2018-07-20 19:32:31 +0900

    parameterize

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import contextlib
 
 app = flask.Flask(__name__)
 
+roles = " PRIMARY SECONDARY".split()
 states = "ONLINE OFFLINE RECOVERING ERROR UNREACHABLE".split()
 
 @app.route("/metrics", methods=["GET"])
@@ -15,12 +16,22 @@ def metrics():
 		password=os.environ["MYSQL_PASSWORD"])
 	with contextlib.closing(con):
 		cur = con.cursor()
-		cur.execute('''SELECT * FROM performance_schema.replication_group_members''')
-		names = [d[0].lower() for d in cur.description]
+		n = cur.execute('''SELECT MEMBER_ID, MEMBER_HOST, MEMBER_STATE, MEMBER_ROLE, MEMBER_VERSION
+			FROM performance_schema.replication_group_members''')
+		names = "MEMBER_ID MEMBER_HOST MEMBER_STATE MEMBER_ROLE MEMBER_VERSION".split()
+		
+		params = {}
 		for r in cur:
-			params = ", ".join(['%s="%s"' % nv for nv in zip(names, r)])
-			state = dict(zip(names, r))["member_state"]
-			lines += "replication_group_members{%s} %d\r\n" % (params, states.index(state))
+			o = dict(zip(names,r))
+			host = o["MEMBER_HOST"]
+			params["%s.state" % host] = o["MEMBER_STATE"]
+			params["%s.role" % host] = o["MEMBER_ROLE"]
+			params["%s.state.index" % host] = states.index(o["MEMBER_STATE"])
+			params["%s.role.index" % host] = roles.index(o["MEMBER_ROLE"])
+		
+		lines += "replication_group_members{%s} %d\r\n" % (
+			", ".join(['%s="%s"' % (k,v) for k,v in params.items()]),
+			n)
 		
 		return lines
 
